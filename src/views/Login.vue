f3e8ae2407adb3bab7f66e64d4d21a714f4ba91c
mather <misaka@misaka.im>
2025-03-03 17:40:05 +0800

    add login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,36 @@
 <script setup>
+  import { ref } from 'vue'
+  const email = ref('')
+  const password = ref('')
+  const isLoading = ref(false)
+  const err = ref(false)
 
+  function handleSubmit (){
+    //e.preventDefault()
+    //login(email, password)
+  }
 </script>
 
 <template>
+  <form class="login" @submit.prevent="handleSubmit">
+    <h3>Login</h3>
 
+    <label>Email:</label>
+    <input
+        type="email"
+        v-model="email"
+    />
+
+    <label>Password:</label>
+    <input
+        type="password"
+        v-model="password"
+    />
+
+    <button :disabled="isLoading">Log in</button>
+
+    <div v-if="err" class="error">{{ error }}</div>
+  </form>
 </template>
 
 <style scoped>
