9e4666a49771d9cdb3e12db53508f74b8254087c
mather <misaka@misaka.im>
2025-03-04 10:53:20 +0800

    add sign up page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,8 +1,35 @@
 <script setup>
+  import { ref } from 'vue'
+  const email = ref('')
+  const password = ref('')
+  const isLoading = ref(false)
+  const err = ref(false)
 
+  function handleSubmit (){
+    //e.preventDefault()
+    //signup(email, password)
+  }
 </script>
 
 <template>
+  <form class="signup" @submit="handleSubmit">
+    <h3>Sign up</h3>
+    <label>Email:</label>
+    <input
+        type="email"
+        v-model="email"
+    />
+
+    <label>Password:</label>
+    <input
+        type="password"
+        v-model="password"
+    />
+
+    <button :disabled="isLoading">Sign up</button>
+
+    <div v-if="err" class="error">{{ error }}</div>
+  </form>
 
 </template>
 
