5649ed56fe7d101856346905cee11aca090e6ddb
mather <misaka@misaka.im>
2025-03-24 22:39:20 +0800

    创建表单当前时间

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -6,10 +6,23 @@ const reps = ref('')
 const error = ref(null)
 const emptyFields = ref([])
 const todo_form = ref({
-  title: '',
-  date: '',
-  description: '',
+  title: '申请奖学金',
+  date: '2025-03-04 16:27',
+  description: '打印表格、按规定填写',
+  createdAt: '',
 })
+function getFormattedDate() {
+  const now = new Date();
+
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始，需要 +1
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
 
 async function handleSubmit() {
   const { title, date, description } = todo_form.value
@@ -17,8 +30,9 @@ async function handleSubmit() {
   if (!title) emptyFields.value.push('title')
   if (!date) emptyFields.value.push('date')
   if (!description) emptyFields.value.push('description')
+  todo_form.value.createdAt = getFormattedDate()
+  console.log('valid: ', todo_form.value)
 
-  console.log('valid: ', emptyFields.value)
 }
 </script>
 
