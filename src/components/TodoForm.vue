ea90f39d24a6d097179afcf40bb39022e4a5ac3b
mather <misaka@misaka.im>
2025-03-24 11:34:08 +0800

    添加表单

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -5,74 +5,83 @@ const load = ref('')
 const reps = ref('')
 const error = ref(null)
 const emptyFields = ref([])
+const todo_form = ref({
+  title: '',
+  date: '',
+  description: '',
+})
 
 async function handleSubmit() {
-  // if(!user) {
-  //   setError('You muse be logged in')
-  //   return
-  // }
-  const user = { token: '123'}
-
-  const workout = {title: title.value, load: load.value, reps: reps.value}
-
-  const response = await fetch('/api/workouts', {
-    method: 'POST',
-    body: JSON.stringify(workout),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authrization': 'Bearer ' + user.token
-    }
-  })
-  const json = await response.json().catch(err => console.log(err))
-
-  if (!response.ok) {
-    if(response.status === 404) { return error.value = 'network err'}
-    error.value = json.error
-    emptyFields.value = json.emptyFields
-  }
-  if (response.ok) {
-    title.value = ''
-    load.value = ''
-    reps.value = ''
-    error.value = null
-    emptyFields.value = []
-    console.log('new workout added', json)
-    //dispatch({type: 'CREATE_WORKOUT', payload: json})
-  }
+  const { title, date, description } = todo_form.value
+  emptyFields.value = [] //clear emptyFields
+  if (!title) emptyFields.value.push('title')
+  if (!date) emptyFields.value.push('date')
+  if (!description) emptyFields.value.push('description')
 }
 </script>
 
 <template>
-  <form class="create" @submit.prevent="handleSubmit" >
+  <form class="create" @submit.prevent="handleSubmit(todo_form)" >
     <h3>添加新的待办事项</h3>
 
     <label>待办事项:</label>
     <input
         type="text"
-        v-model="title"
+        v-model="todo_form.title"
         :class="{ 'error' : emptyFields.includes('title') }"
     />
 
     <label>时间:</label>
     <input
         type="datetime-local"
-        v-model="load"
-        :class="{ 'error' : emptyFields.includes('load') }"
+        v-model="todo_form.date"
+        :class="{ 'error' : emptyFields.includes('date') }"
     />
 
     <label>详细内容:</label>
     <input
         type="text"
-        v-model="reps"
-        :class="{ 'error' : emptyFields.includes('reps') }"
+        v-model="todo_form.description"
+        :class="{ 'error' : emptyFields.includes('description') }"
     />
 
     <button>添加</button>
-
     <div v-if="error" class="error">{{ error }}</div>
   </form>
 </template>
 
 <style scoped>
+label, input {
+  display: block;
+}
+input {
+  padding: 10px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  width: 100%;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+form button {
+  background: var(--primary);
+  border: 0;
+  color: #fff;
+  padding: 10px;
+  font-family: "Poppins";
+  border-radius: 4px;
+  cursor: pointer;
+}
+div.error {
+  padding: 10px;
+  background: #ffefef;
+  border: 1px solid var(--error);
+  color: var(--error);
+  border-radius: 4px;
+  margin: 20px 0;
+}
+input.error {
+  border: 1px solid var(--error);
+}
 
 </style>
