9885d58a3a8085a47c1f9b8c23285c5b6f40b31d
mather <misaka@misaka.im>
2025-04-17 15:57:36 +0800

    登录注册页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,36 @@
 <script setup>
+import { ref } from 'vue'
+const email = ref('')
+const password = ref('')
+const isLoading = ref(false)
+const err = ref(false)
 
+function handleSubmit (){
+  //e.preventDefault()
+  //login(email, password)
+}
 </script>
 
 <template>
+  <form class="login" @submit.prevent="handleSubmit">
+    <h3>登录</h3>
 
+    <label>邮箱:</label>
+    <input
+        type="email"
+        v-model="email"
+    />
+
+    <label>密码:</label>
+    <input
+        type="password"
+        v-model="password"
+    />
+
+    <button :disabled="isLoading">登录</button>
+
+    <div v-if="err" class="error">{{ error }}</div>
+  </form>
 </template>
 
 <style scoped>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,8 +1,35 @@
 <script setup>
+import { ref } from 'vue'
+const email = ref('')
+const password = ref('')
+const isLoading = ref(false)
+const err = ref(false)
 
+function handleSubmit (){
+  //e.preventDefault()
+  //signup(email, password)
+}
 </script>
 
 <template>
+  <form class="signup" @submit="handleSubmit">
+    <h3>注册</h3>
+    <label>邮箱:</label>
+    <input
+        type="email"
+        v-model="email"
+    />
+
+    <label>密码:</label>
+    <input
+        type="password"
+        v-model="password"
+    />
+
+    <button :disabled="isLoading">注册</button>
+
+    <div v-if="err" class="error">{{ error }}</div>
+  </form>
 
 </template>
 
